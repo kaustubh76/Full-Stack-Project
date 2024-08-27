2948aa8c956ac685518bc3da5b7050323b23313b
Kaushtubh Agrawal <21ucc059@lnmiit.ac.in>
2024-08-27 18:37:02 +0530

    Added more router

--- src/middleware.ts
+++ src/middleware.ts
@@ -20,7 +20,11 @@ export async function middleware(request: NextRequest) {
         return NextResponse.redirect(new URL('/dashboard', request.url))
     }
 
-  return NextResponse.redirect(new URL('/home', request.url))
+    if(!token && url.pathname.startsWith('/dashboard')){
+        return NextResponse.redirect(new URL('/sign-in', request.url));
+    }
+
+  return NextResponse.next()
 }
  
 // See "Matching Paths" below to learn more
@@ -29,8 +33,8 @@ export const config = {
     '/sign-in',
     '/sign-up',
     '/home',
-    '/'
-//    'dashboard/:path*',
-//     '/verify/:path*'
+    '/',
+   'dashboard/:path*',
+    '/verify/:path*'
 ]
 }
